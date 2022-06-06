8cfd321ea6c5620f19a2702a4e9146a9f98c13ec
Matias Wengiel <matiaswengiel@gmail.com>
2022-06-06 14:04:55 -0500

    Update schema with improved constraints from example

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -30,18 +30,18 @@ CREATE TABLE properties (
 DROP TABLE IF EXISTS reservations CASCADE;
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE,
-  end_date DATE,
-  property_id INTEGER REFERENCES properties(id),
-  guest_id INTEGER REFERENCES users(id)
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS property_reviews CASCADE;
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  guest_id INTEGER REFERENCES users(id),
-  property_id INTEGER REFERENCES properties(id),
-  reservation_id INTEGER REFERENCES reservations(id),
-  rating SMALLINT CHECK(rating >= 5),
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0 CHECK(rating >= 5),
   message TEXT
 );
