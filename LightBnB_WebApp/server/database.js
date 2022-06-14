a857e52a44349a73592424bd62a77ef638baf87f
Matias Wengiel <matiaswengiel@gmail.com>
2022-06-14 11:16:07 -0500

    Refactor getUsersWithId to work using SQL database rather than in-memory database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -36,7 +36,13 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then((result) => {
+    return result.rows.length > 0 ? result.rows[0] : null
+    
+  })
+  .catch((err) => {console.log(err.message)
+  })
 }
 exports.getUserWithId = getUserWithId;
 
